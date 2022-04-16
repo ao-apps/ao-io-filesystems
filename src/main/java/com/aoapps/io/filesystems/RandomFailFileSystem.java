4b541a98980ecac95c3295a45b1863d68d9277ca
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:49:32 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/io/filesystems/RandomFailFileSystem.java
+++ src/main/java/com/aoapps/io/filesystems/RandomFailFileSystem.java
@@ -1,6 +1,6 @@
 /*
  * ao-io-filesystems - Advanced filesystem utilities.
- * Copyright (C) 2015, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2015, 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-io-filesystems.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.io.filesystems;
 
 import com.aoapps.lang.Throwables;
